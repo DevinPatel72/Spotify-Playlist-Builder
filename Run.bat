f00c255172aac69cf1e92048b1baa6820f87bb80
DevinPate1 <90291312+DevinPate1@users.noreply.github.com>
2021-11-28 21:25:23 -0600

    Delete Run.bat

--- Run.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Users\xdp20\OneDrive\Assignments and Projects\HON 301 - Rapid Prototyping\SpotifySwipe\Scripts\pythonw.exe" "C:\Users\xdp20\OneDrive\Assignments and Projects\HON 301 - Rapid Prototyping\SpotifySwipe\app.py"
